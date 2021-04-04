259773a68ac7e228c846ee26f004bbff566bc44c
Ben Vernazza <nazzer23@hotmail.co.uk>
2021-04-04 12:28:09 +0100

    Started TCP Server

--- Core.cs
+++ Core.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
@@ -15,6 +16,7 @@ namespace Game
         public static readonly string programStartTime = DateTime.Now.ToFileTime().ToString();
         public static bool isRunning = true;
         private TcpListener socketTcpListener = null;
+        public bool canListen = true;
 
         public static Core GetInstance()
         {
@@ -62,6 +64,31 @@ namespace Game
 
             var serverPort = DatabaseHandler.GetInstance().Fetch($"SELECT * FROM servers WHERE Name LIKE '{ConfigHandler.GetInstance().GetString("serverName")}'").Rows[0]["Port"];
             LogHandler.GetInstance().Log("Starting server on port " + serverPort, LogType.INFO);
+            Listen((Int32)serverPort);
+        }
+
+        private void Listen(int port)
+        {
+            this.socketTcpListener = new TcpListener(new IPEndPoint(IPAddress.Any, port));
+            this.socketTcpListener.Start();
+            LogHandler.GetInstance().Log($"Listening on {this.socketTcpListener.LocalEndpoint}", LogType.SUCCESS);
+            int counter = 0;
+            while (this.socketTcpListener.Server.IsBound && this.canListen)
+            {
+                if (this.socketTcpListener.Pending())
+                {
+                    TcpClient client = this.socketTcpListener.AcceptTcpClient();
+                    LogHandler.GetInstance().Log($"Accepting client {counter}", LogType.INFO);
+
+                    //byte[] writeBytes = Encoding.ASCII.GetBytes("OMG, MAPRIL IT WORKS");
+
+                    //NetworkStream nw = client.GetStream();
+                    //nw.Write(writeBytes, 0, writeBytes.Length);
+                    //nw.Flush();
+
+                    counter++;
+                }
+            }
         }
 
         public void Error(string message)
